1c96095c9859f5ea9c99ff049d213707ae4bf098
Gopal <gopalakrishna.gunji@gmail.com>
2026-06-15 03:27:45 -0400

    added few lines

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -3,3 +3,7 @@ echo "Hello, World!"
 echo "Welcome to Shell Scripting Practice!"
 echo "this is Durga Gopala Krishna Gunji"
 # this is comment introduction to shell scripting
+echo "This is a simple shell script to print messages to the console."
+echo "You can run this script using the command: bash 01-hello-world.sh"
+echo "Feel free to modify this script and experiment with different commands!"
+echo "Happy scripting!"
