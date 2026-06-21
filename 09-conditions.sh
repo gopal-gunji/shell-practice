7ffcf0b6c23955ab915ce84d45a1a17285cc9ebb
Gopal <gopalakrishna.gunji@gmail.com>
2026-06-21 00:51:56 -0400

    actions

---
+++ 09-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER=25
+
+#-gt -> greater than
+# -lt -> less than
+# -eq -> equal to
+# -ne -> not equal to 
+
+if [ $NUMBER -gt 20]; then 
+    echo "Given Number is : $NUMBER is greater than 20"
+fi
