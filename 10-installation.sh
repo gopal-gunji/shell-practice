d7d73df7d03dfdc93d3df4e0023ef816ab70cab7
Gopal <gopalakrishna.gunji@gmail.com>
2026-06-21 01:15:04 -0400

    actions

--- 10-installation.sh
+++ 10-installation.sh
@@ -5,8 +5,15 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then 
     echo " please run this script with root user access"
-
+    exit 1
 fi
 
 echo "installing Nginx web server"
 dnf install nginx -y
+
+if [ $? -ne 0 ]; then 
+    echo "Installing Nginx web server failed"
+    exit 1
+else
+    echo "Nginx web server installed successfully"
+fi
