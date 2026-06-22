bced10a43330408c6003719ffab7174067849d53
Gopal <gopalakrishna.gunji@gmail.com>
2026-06-22 00:31:24 -0400

    actions

--- 10-installation.sh
+++ 10-installation.sh
@@ -17,3 +17,31 @@ if [ $? -ne 0 ]; then
 else
     echo "Nginx web server installed successfully"
 fi
+
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then 
+    echo "Installing MySQL failed"
+    exit 1
+else
+    echo "MySQL installed successfully"
+fi
+
+dnf insall php -y
+
+if [ $? -ne 0 ]; then 
+    echo "Installing php failed"
+    exit 1
+else
+    echo "php installed successfully"
+fi
+
+dnf install nodejs -y
+
+if [ $? -ne 0 ]; then 
+    echo "Installing nodejs failed"
+    exit 1
+else 
+    echo "nodejs installed successfully"
+fi
