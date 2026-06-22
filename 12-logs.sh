6c9ac23b0df69608c2f47b27cacf8d47c5397a57
Gopal <gopalakrishna.gunji@gmail.com>
2026-06-22 01:33:58 -0400

    actions

--- 12-logs.sh
+++ 12-logs.sh
@@ -6,7 +6,7 @@ LOGS_FOLDER="/var/log/shell-script"
 LOG_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then 
-    echo " please run this script with root user access"
+    echo " please run this script with root user access" | tee -a $LOG_FILE
     exit 1
 fi
 
@@ -14,10 +14,10 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "$2 ....failed"
+        echo "$2 ....failed" | tee -a $LOG_FILE
         exit 1
     else
-        echo "$2 ....success"
+        echo "$2 ....success" | tee -a $LOG_FILE
     fi
 }
 
