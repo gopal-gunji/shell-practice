e6cef9fe0129ca147eab9d82d28ba733dc93a4cc
Gopal <gopalakrishna.gunji@gmail.com>
2026-06-22 02:47:02 -0400

    actions

---
+++ 16-colors.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+M="\e[35m"
+C="\e[36m"
+W="\e[37m"
+N="\e[0m"
+
+echo -e "$R Hello, World! $N, $Y Welcome to Shell Scripting Practice! $N, $G this is Durga Gopala Krishna Gunji $N"
+
+
+echo -e "${R}This is Red Color${N}"
+echo -e "${G}This is Green Color${N}"
+echo -e "${Y}This is Yellow Color${N}"
+echo -e "${B}This is Blue Color${N}"        
+echo -e "${M}This is Magenta Color${N}"
+echo -e "${C}This is Cyan Color${N}"
+echo -e "${W}This is White Color${N}"   
+
