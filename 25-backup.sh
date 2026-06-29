4c7df32c0029ff593fb354f703ae23e505f44aa3
Gopal <gopalakrishna.gunji@gmail.com>
2026-06-29 02:47:45 -0400

    actions

--- 25-backup.sh
+++ 25-backup.sh
@@ -52,4 +52,10 @@ log "Days: $DAYS"
 
 if [ -z $FILES ]; then
     log "No files to archieve ... $Y Skipping $N"
+else
+    # app-logs- time stamp
+    log "Files found to archive: $FILES"
+    TIMESTAMP=$(date +%F-%H-%M-%S)
+    ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    echo "Archieve name: $ZIP_FILE_NAME"
 fi
