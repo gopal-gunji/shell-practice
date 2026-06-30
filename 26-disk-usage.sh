34c90281832f91891d209c0a8b0372f6d5fb0cc6
Gopal <gopalakrishna.gunji@gmail.com>
2026-06-30 03:31:34 -0400

    actions

--- 26-disk-usage.sh
+++ 26-disk-usage.sh
@@ -4,24 +4,27 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+MESSAGE=""
 IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 log(){
-    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE 
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1"
 }
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 USAGE_THRESHOLD=3
+
 while IFS= read -r line
 do
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
-    PARTITION+=$(echo $line | awk '{print $7}')
+    PARTITION=$(echo $line | awk '{print $7}')
 
-    if [ "$USAGE" -gt "$USAGE_THRESHOLD" ]; then
-        MESSAGE+="High DISK USAGE On $PARTITION:$USAGE% \n"
+    if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
+        MESSAGE+="High Disk usage on $PARTITION: $USAGE% <br>"
     fi
 done <<< $DISK_USAGE
 
 echo -e "$MESSAGE"
+
 
 sh mail.sh "gopalakrishna.gunji@gmail.com" "High Disk Usage alert On $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
--- mail.sh
+++ mail.sh
@@ -5,13 +5,13 @@ MESSAGE_BODY=$3
 ALERT_TYPE=$4
 SERVER_IP=$5
 TO_TEAM=$6
-FINAL_MESSAGE_BODY=$( echo $MESSAGE_BODY | sed -e 's/[]\/$*.^[]/\\&/g')
+FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed -e 's/[]\/$*.^[]/\\&/g')
 
-FINAL_MESSAGE=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP" -e "s/FINAL_MESSAGE_BODY/g" templete.html)
+FINAL_MESSAGE=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g" template.html)
 
 {
 echo "To: $TO_ADDRESS"
-echo "Subject: $SUBJECT "
+echo "Subject: $SUBJECT"
 echo "Content-Type: text/html"
 echo ""
 echo "$FINAL_MESSAGE"
