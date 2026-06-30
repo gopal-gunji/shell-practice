c7f8f4568f5649657fc5b6f58a643b4cfd83fdf4
Gopal <gopalakrishna.gunji@gmail.com>
2026-06-30 03:26:51 -0400

    actions

--- mail.sh
+++ mail.sh
@@ -5,14 +5,14 @@ MESSAGE_BODY=$3
 ALERT_TYPE=$4
 SERVER_IP=$5
 TO_TEAM=$6
-FINAL_MESSAGE_BODY=$( echo $MESSAGE_BODY | sed -e "s/'/'\\\''/g")
+FINAL_MESSAGE_BODY=$( echo $MESSAGE_BODY | sed -e 's/[]\/$*.^[]/\\&/g')
 
 FINAL_MESSAGE=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP" -e "s/FINAL_MESSAGE_BODY/g" templete.html)
 
 {
-echo "To: gopalakrishna.gunji@gmail.com"
-echo "Subject: Shell Scripting Testing "
+echo "To: $TO_ADDRESS"
+echo "Subject: $SUBJECT "
 echo "Content-Type: text/html"
 echo ""
 echo "$FINAL_MESSAGE"
-} | msmtp "gopalakrishna.gunji@gmail.com"
+} | msmtp "$TO_ADDRESS"
